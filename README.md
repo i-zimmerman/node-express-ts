b356b9d00164e38b3ff582d96091ad5394d9ffae
alex zimmerman <alex.zimmerman.info@gmail.com>
2021-11-06 22:01:20 +0300

    crud todos

---
+++ README.md
@@ -0,0 +1,1 @@
+### Simple crud node.js app with express
---
+++ src/app.ts
@@ -0,0 +1,15 @@
+import express, { NextFunction, Response, Request } from "express";
+import todoRoutes from "./routes/todos";
+import { json } from "body-parser";
+
+const app = express();
+
+// make sure to populate req body with the parsed json data
+app.use(json());
+app.use("/todos", todoRoutes);
+
+app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+  res.status(500).json({ message: err.message });
+});
+
+app.listen(3000);
